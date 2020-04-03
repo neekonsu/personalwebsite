052cb28cfd4293cdc83aad52db50f303e6d4efcb
Neekon Saadat <neekonsu@gmail.com>
2020-04-03 16:52:05 -0700

    not working, to be debugged

--- main/main.go
+++ main/main.go
@@ -8,9 +8,9 @@ import (
 )
 
 func main() {
-	html := flag.String("-html-path", "./template.html", "Path to template html file")
-	json := flag.String("-json-path", "./sitemap.json", "Path to sitemap json file")
-	cpPath := flag.String("-cp", "./colorpalette.json", "Path to colorpalette json file")
+	html := flag.String("-html-path", "./res/template.html", "Path to template html file")
+	json := flag.String("-json-path", "./res/portfolio.json", "Path to sitemap json file")
+	cpPath := flag.String("-cp", "./res/colorpalette.json", "Path to colorpalette json file")
 	handler := personalwebsite.HandleSitemap(*html, *json, *cpPath)
-	http.ListenAndServe(":20", handler)
+	http.ListenAndServe(":8080", handler)
 }
